2e04e603ca4eeb6fa68b5349d2665fe08ea84b0c
Siranyang <406625769@qq.com>
2021-01-06 18:08:19 +0800

    Revert "joining to zhz server."
    This reverts commit b581a034f2868a03f5a13614b02bd841e7da8458.

--- src/ui/screens/AutoLoginScreen.tsx
+++ src/ui/screens/AutoLoginScreen.tsx
@@ -7,13 +7,12 @@ import {
     useNavigationState,
 } from '@react-navigation/native';
 
-import { NativeEventEmitter, NativeModules, Image, Dimensions, ImageBackground, StatusBar, AppState } from 'react-native';
+import { NativeEventEmitter, NativeModules, Image, Dimensions, ImageBackground, StatusBar } from 'react-native';
 
 import { View, ToastAndroid, BackHandler, Platform, Text, Button, Alert, StyleSheet, TouchableHighlight } from 'react-native';
 import autojs from '../../autojs/autojs';
 import requestMultiplePermission from '../../rnservices/QueryPermission';
 import { ScrollView, TextInput, TouchableWithoutFeedback } from 'react-native-gesture-handler';
-import { resolvePlugin } from '@babel/core';
 
 class AutologinScreen extends React.Component<NavigationContainerProps> {
     state = { log: "", scriptStat: "尚未运行", loginSha1: "" };
@@ -23,72 +22,43 @@ class AutologinScreen extends React.Component<NavigationContainerProps> {
 
     constructor(props: Readonly<NavigationContainerProps<{}, {}, unknown>>) {
         super(props);
-        this.state = {log: "", scriptStat: "尚未运行", loginSha1: typeof(this.props.navigation?.state?.params?.order)=='undefined' ? "" : this.props.navigation?.state?.params?.order};
-        console.log("at construction, logsha1 is " + this.state.loginSha1);
     }
+
 
     componentDidMount() {
         StatusBar.setBackgroundColor('transparent');
         StatusBar.setTranslucent(true);
     }
 
-    /*componentDidUpdate() {
-        console.log("UNSAFE_componentWillReceiveProps called " + this.props.navigation?.state?.params?.order);
-        let tempid = this.props.navigation?.state?.params?.order;
-        if(typeof(tempid) != 'undefined' && this.state.loginSha1 != tempid) {
-            this.setState({loginSha1: tempid});
-        }
-    }*/
+    private parseSha1(sha1:string) {
+        if(sha1 == '6744b7d35b34bb943419086'){
+            console.log("get right sha1,will return tokens.");
+            return {'errorcode': 'OK',
+                    'openid': '6A33DDDE887526C4DBB2A9ECEC15500F',
+                    'atoken': '6F5E4BA8BF4D8525147FF04813C3B3D0',
+                    'ptoken': '1E13CAFACBD9C8699E5BF20697698195'};
+        } else if (sha1 == '123456') {
 
-    static getDerivedStateFromProps(nextProps:NavigationContainerProps ,prevState) {
-        let tempid = nextProps.navigation?.state?.params?.order;
-        if(typeof(tempid) != 'undefined' && prevState.loginSha1 != tempid) {
-            return {loginSha1: tempid};
+            return {'errorcode': 'OK',
+                    'openid': '2B1F8819499A2C8C4062BB1859F3421B',
+                    'atoken': '88E39ED743C9FDB422229D8B6DA4C294',
+                    'ptoken': 'CBF9A17DFFEBF098273E1261C9988286'};
+        } else {
+            console.log("invalidate,will return null.");
+            return {'errorcode': 'failed',};
         }
-        return null;
     }
 
-    private async parseSha1(sha1:string) {
-        var result;
-        await fetch('http://zuhaozhuang.com/api/unified.aspx?key=e6xrW0t6K79bWlt6K79bXE&a=tokenQuery&id=' + sha1, {method: 'GET'})
-            .then((response) => response.json())
-            .then((responseJson) => {
-                console.log('get correct json');
-                console.log(responseJson);
-
-                if(responseJson.code != 1) {
-                    result = {'errorcode': 'failed'};
-                } else {
-                result = {'errorcode': 'OK',
-                'openid': JSON.parse(responseJson.tokendata).openid,
-                'atoken': JSON.parse(responseJson.tokendata).atoken,
-                'ptoken': JSON.parse(responseJson.tokendata).ptoken,};
-                }
-            })
-            .catch(e => {
-                console.log(`error ${e}`);
-                console.log('catch exception');
-                return Promise.resolve({'errorcode': 'failed'});
-            });
-
-            return(result);
-    }
-
-    public async onClickStartLogin() {
+    public onClickStartLogin() {
         console.log('loginsha1 is[' + this.state.loginSha1 + ']' );
-        console.log("at onClickStartLogin, order is " + this.props.navigation?.state?.params?.order);
-        if (this.state.loginSha1 == "" || typeof (this.state.loginSha1) == 'undefined') {
+        if (this.state.loginSha1 == "" ) {
             ToastAndroid.show('请输入解锁码', ToastAndroid.SHORT);
             return;
         }
 
-        //await requestMultiplePermission(); //current functions not need dangerous permission.
-        var result;
-        await this.parseSha1(this.state.loginSha1).then(v => {result = v});
-        console.log("result is " + JSON.stringify(result));
-
+        //await requestMultiplePermission();
+        let result = this.parseSha1(this.state.loginSha1);
         if(result.errorcode== 'OK' ) {
-            //ToastAndroid.show("openid:" + result.openid + ",atoken:" + result.atoken + ",ptoken" + result.ptoken, ToastAndroid.SHORT);
             autojs.loginByTokens(result.openid, result.atoken, result.ptoken);
         } else {
             ToastAndroid.show('请输入正确解锁码', ToastAndroid.SHORT);
@@ -115,7 +85,7 @@ class AutologinScreen extends React.Component<NavigationContainerProps> {
                     </View>
 
                     <View style={{ backgroundColor: '#666666', opacity: 0.8, width: this.win.width, alignItems: 'center', justifyContent: 'center', height: 80 }}>
-                        <TextInput style={{ flex: 1, alignItems: 'center', justifyContent: 'center', fontSize: 20, color: '#ffffff', width: this.win.width, textAlign: "center"}}
+                        <TextInput style={{ flex: 1, alignItems: 'center', justifyContent: 'center', fontSize: 20, color: '#ffffff' }}
                             placeholder={'请输入游戏解锁码'}
                             onChangeText={(sha1) => this.setState({ loginSha1: sha1 })}
                             value={this.state.loginSha1}
@@ -128,7 +98,7 @@ class AutologinScreen extends React.Component<NavigationContainerProps> {
                         </Text>
                     </TouchableHighlight>
 
-                    <View style={{ width: 500, height: this.win.height * 2 / 5 }}>
+                    <View style={{ width: 500, height: this.win.height * 3 / 5 }}>
                     </View>
 
                     <View style={{ alignItems: 'center' }}>
